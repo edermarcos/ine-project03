14e7d69f789ea2eac091d4c6135139b5aae61ad9
Eder Marcos <eder.marcos.lara@gmail.com>
2023-09-05 00:21:50 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ine-project03';
+export class AppComponent implements OnInit {
+  backupBooks: Book[] = [];
+  books: Book[] = [];
+  inputSearch: string = '';
+
+  constructor(private http: HttpClient) {
+    this.getBooks();
+  }
+
+  ngOnInit(): void {
+  }
+
+  handleSearch(): void {
+    this.books = this.backupBooks.filter(book => book.titulo.includes(this.inputSearch));
+  }
+
+  resetSearch(): void {
+    this.inputSearch = '';
+    this.books = this.backupBooks;
+  }
+
+  getBooks(): void {
+    this.http.get<Book[]>('/assets/books.json').subscribe(data => {
+      this.backupBooks = data;
+      this.books = data;
+    });
+  }
+}
+
+export interface Book {
+  id: number;
+  titulo: string;
+  autor: string;
+  editorial: string;
+  descripcion: string;
+  imagen: string;
 }
